215ee6ea67c2650e011860ef39ad828f1cba0f3e
AntoineChatry <antoine.chatry78@orange.fr>
2022-12-17 03:50:54 +0100

    Create IngredientController.php
    CRUD Recipe & Ingredient + Add security

---
+++ src/Controller/IngredientController.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\IngredientRepository;
+use App\Entity\Ingredient;
+use App\Form\IngredientType;
+use Doctrine\ORM\EntityManagerInterface;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+
+class IngredientController extends AbstractController
+{
+    /**
+     * This controller display all ingredients
+     *
+     * @param IngredientRepository $repository
+     * @param PaginatorInterface $paginator
+     * @param Request $request
+     * @return Response
+     */
+    #[Route('/ingredient', name: 'ingredient', methods: ['GET'])]
+    #[IsGranted('ROLE_USER')]
+    public function index(IngredientRepository $repository, PaginatorInterface $paginator, Request $request): Response
+    {
+        $ingredients = $paginator->paginate(
+            $repository->findBy(['user' => $this->getUser()]),
+            $request->query->getInt('page', 1),
+            10
+        );
+        return $this->render('pages/ingredient/index.html.twig', [
+            'ingredients' => $ingredients
+        ]);
+    }
+    /**
+     * Create a new ingredient
+     *
+     * @param Request $request
+     * @param EntityManagerInterface $manager
+     * @return Response
+     */
+    #[Route('/ingredient/nouveau', name: 'ingredient.new', methods: ['GET', 'POST'])]
+    #[IsGranted('ROLE_USER')]
+    public function new(Request $request, EntityManagerInterface $manager): Response
+    {
+        $ingredient = new Ingredient();
+        $form = $this->createForm(IngredientType::class, $ingredient);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $ingredient = $form->getData();
+            $ingredient->setUser($this->getUser());
+            $manager->persist($ingredient);
+            $manager->flush();
+            $this->addFlash('success', 'L\'ingrédient a bien été ajouté');
+            return $this->redirectToRoute('ingredient');
+        }
+
+
+        return $this->render('pages/ingredient/new.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+    /**
+     * Edit an Ingredient
+     *
+     * @param Ingredient $ingredient
+     * @param Request $request
+     * @param EntityManagerInterface $manager
+     * @return Response   
+     */
+    #[Route('/ingredient/edition/{id}', name: 'ingredient.edit', methods: ['GET', 'POST'])]
+    #[Security("is_granted('ROLE_USER') and user === ingredient.getUser()")]
+    public function edit(Ingredient $ingredient, Request $request, EntityManagerInterface $manager): Response
+    {
+        $form = $this->createForm(IngredientType::class, $ingredient);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $ingredient = $form->getData();
+            $manager->persist($ingredient);
+            $manager->flush();
+            $this->addFlash('success', 'L\'ingrédient a bien été modifié');
+            return $this->redirectToRoute('ingredient');
+        }
+        return $this->render('pages/ingredient/edit.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+    /**
+     * Delete an ingredient
+     *
+     * @param Ingredient $ingredient
+     * @param EntityManagerInterface $manager
+     * @return Response
+     */
+    #[Route('/ingredient/suppression/{id}', name: 'ingredient.delete', methods: ['GET'])]
+
+    public function delete(Ingredient $ingredient, EntityManagerInterface $manager): Response
+    {
+        if (!$ingredient) {
+            $this->addFlash('warning', 'L\'ingrédient n\'existe pas');
+            return $this->redirectToRoute('ingredient');
+        }
+        $manager->remove($ingredient);
+        $manager->flush();
+        $this->addFlash('success', 'L\'ingrédient a bien été supprimé');
+        return $this->redirectToRoute('ingredient');
+    }
+}
